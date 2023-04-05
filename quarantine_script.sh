a144422113d8e424ffd1409afbd7c7e62e24a059
tanmayd004 <72381262+tanmayd004@users.noreply.github.com>
2023-04-05 16:04:26 +0530

    Create quarantine_script.sh

---
+++ quarantine_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script should take an endpoint name as an argument and perform the necessary actions to quarantine it.
+
+endpoint=$1
+
+# Perform quarantine actions on endpoint here
+
+# For example, you can disconnect the endpoint from the network, disable user accounts, etc.
+
+echo "Endpoint $endpoint has been quarantined."
+
